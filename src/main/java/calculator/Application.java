9ec2d0fb1936df9272a3fd19d6cea8969963d8c0
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-10-15 18:31:27 +0900

    feat: 문자열 입력 기능 추가 및 기본 애플리케이션 로직 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,41 @@
 package calculator;
 
+import calculator.parser.NumberParser;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Calculator calculator = new Calculator();
+        int result;
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        try {
+            String input = Console.readLine();
+            System.out.println("입력받은 문자열 : " + input);
+
+            if (input.isEmpty()) {
+                result = 0;
+            }
+
+            else {
+                int[] numbers = new int[input.length()];
+
+                NumberParser parser = new NumberParser();
+                numbers = parser.parseNumbers(input);
+
+                for (int i = 0; i < numbers.length; i++) {
+                    System.out.println("파싱된 숫자 : " + numbers[i]);
+                }
+
+                result = calculator.calculate(numbers);
+
+            }
+            System.out.println("계산 결과 : " + result);
+        }
+
+        catch (IllegalStateException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
