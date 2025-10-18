974f1ee3109ea45be014accd91072c3f7ec0804e
DaeunSon <130820902+DaeunSon@users.noreply.github.com>
2025-10-18 11:36:28 +0900

    fix : 기본 구분자 오탈자 수정

--- src/main/java/calculator/parser/DefaultNumberParser.java
+++ src/main/java/calculator/parser/DefaultNumberParser.java
@@ -4,7 +4,13 @@ public class DefaultNumberParser implements NumberParser {
 
     @Override
     public int[] parseNumbers(String input) {
-        String[] strNumbers = input.split(",|;");
+        if (input == null) {
+            throw new IllegalArgumentException("유효하지 않은 입력 : 입력이 존재하지 않습니다.");
+        }
+        if (input.isEmpty()) {
+            return new int[0];
+        }
+        String[] strNumbers = input.split(",|:");
 
         int[] numbers = new int[strNumbers.length];
 
